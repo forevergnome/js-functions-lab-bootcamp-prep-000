85fe4edf3aba129c5fbc807e9cfdd623a244f9e6
Megan Hudson <meganhudson18@gmail.com>
2018-05-26 19:00:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
   
 function holidayCountdown(holiday, days) {
-  return `"It's ${days} days unitl ${holiday}!"`
+  return `It's ${days} days until ${holiday}!`
 }
