a7750c00fc45c7772a5025dee11ebd4d39464e0f
Megan Hudson <meganhudson18@gmail.com>
2018-05-26 18:51:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name) {
-  ret(`Happy holidays, ${name}!`)
+  return `Happy holidays, ${name}!`
   }
   
 function happyHolidayTo(holiday, name) {
-  console.log(`Happy ${holiday}, ${name}!`)
+  return `Happy ${holiday}, ${name}!`
 }
   
